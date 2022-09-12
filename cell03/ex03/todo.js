659b5e645edf8f5a64826fbbe2aa7651c107f291
Passamon Sukmaksin <passamon.smk@gmail.com>
2022-09-12 11:28:00 +0700

    fix todo

--- cell03/ex03/todo.js
+++ cell03/ex03/todo.js
@@ -1,24 +1,25 @@
 let ckies = document.cookie
-    let list = document.getElementById("ft_list");
-    console.log(ckies.length)
-    if (ckies.length > 0) {
-        let ckie = ckies.split(";")
-        ckie.forEach(element => {
-            const newNode = document.createElement("div");
-            newNode.innerHTML = element.split("=")[1]
-            newNode.onclick = ()=>{
-                let remove = confirm("Do you want to remove this to-do?")
-                if (remove) {
-                    newNode.remove()
-                    console.log(element.split("=")[0])
-                    document.cookie = element.split("=")[0] + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC;";
-                }
+let list = document.getElementById("ft_list");
+console.log(ckies)
+if (ckies.length > 0) {
+    let ckie = ckies.split(";")
+    ckie.forEach(element => {
+        const newNode = document.createElement("div");
+        newNode.innerHTML = element.split("=")[1]
+        newNode.onclick = ()=>{
+            let remove = confirm("Do you want to remove this to-do?")
+            if (remove) {
+                newNode.remove()
+                console.log(element.split("=")[0])
+                document.cookie = element.split("=")[0] + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC;";
             }
-            list.prepend(newNode);
-        });
-    }
-    function newTodo() {
-        let todo = prompt("Create a new To-do")
+        }
+        list.prepend(newNode);
+    });
+}
+function newTodo() {
+    let todo = prompt("Create a new To-do")
+    if (todo != null && todo != "") {
         let name = new Date().getTime()
         document.cookie = name + "=" + todo;
         const newNode = document.createElement("div");
@@ -32,3 +33,4 @@ let ckies = document.cookie
             }
             list.prepend(newNode);
     }
+}
